ff911681dedaac3768276628947c22fc09707146
DeywReyzer <114655088+DeywReyzer@users.noreply.github.com>
2023-10-24 12:15:33 +0300

    Add files via upload

---
+++ Lab_6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N;
+    cout << "Enter a natural number: ";
+    cin >> N;
+
+    string numstr = to_string(N);
+    reverse(numstr.begin(), numstr.end());
+
+    cout << "Reversed number: " << numstr << endl;
+
+    return 0;
+}
